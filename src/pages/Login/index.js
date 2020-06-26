338042c19051406b432d877a2c345c38108e5f7e
Marcos <marcos.cursino@hotmail.com>
2020-06-26 18:21:01 -0300

    firt commit

---
+++ src/pages/Login/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+import api from '../../services/api'
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  
+
+async function handleSubimit(event) {
+  event.preventDefault();
+
+  const options = {
+    method: 'post',
+    url   : '/api/v1/auth/login',
+    data  : {
+      email,password }
+  };
+
+   const response = await api(options);
+   console.log(response.data.data.token);
+
+
+}
+
+  return (
+    <>
+    <form onSubmit={(e) => handleSubimit(e)}>
+     <label htmlFor="email">E-mail</label>
+      <input type="email"
+      placeholder="Seu E-mail"
+      onChange={event => setEmail(event.target.value)}
+     
+  
+      />
+
+      <label htmlFor="password">Senha</label>
+      <input type="password"
+      placeholder="Sua senha"
+      onChange={event => setPassword(event.target.value)}
+      
+      
+      />
+      <button className= "btn" type="submit" >Logar</button>
+    </form>
+    </>
+  );
+ }
